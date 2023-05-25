7bf817ea92b519d9ad03a8a223bfde99e4b751de
Vishal Jadav <124347933+VishalJadav23@users.noreply.github.com>
2023-05-26 00:09:01 +0530

    Add files via upload

---
+++ COUNTER/js/functions.js
@@ -0,0 +1,24 @@
+// DOM Element's
+const counters = document.querySelectorAll('.counter');
+
+/*** Same functionality, now using for...of ***/
+
+for (let n of counters) {
+    const updateCount = () => {
+        const target = +n.getAttribute('data-target');
+        const count = +n.innerText;
+        const divider = 1000;
+        const speed = 1; // 1000 millisecond => 1 second;
+
+        const inc = target / divider;
+
+        if (count < target) {
+            n.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, speed);
+        } else {
+            n.innerText = target;
+        }
+    };
+
+    updateCount();
+}
